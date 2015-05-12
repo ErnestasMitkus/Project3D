207d48feeab6ba4219a71a4c5029428b60fae115
Ernestas Mitkus <ernestas@inventi.lt>
2015-05-12 18:28:22 +0300

    Add Raw model, Loader and Renderer

--- project-first/src/main/java/com/ernestas/Main.java
+++ project-first/src/main/java/com/ernestas/Main.java
@@ -1,18 +1,40 @@
 package com.ernestas;
 
 import com.ernestas.renderEngine.DisplayManager;
+import com.ernestas.renderEngine.Loader;
+import com.ernestas.renderEngine.RawModel;
+import com.ernestas.renderEngine.Renderer;
 import org.lwjgl.opengl.Display;
 
 public class Main {
 
     public static void main(String args[]) {
+
         DisplayManager.createDisplay();
 
-        while (!Display.isCloseRequested()) {
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
 
+        float[] vertices = {
+            -0.5f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f,
+
+            0.5f, -0.5f, 0f,
+            0.5f, 0.5f, 0f,
+            -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
+        while (!Display.isCloseRequested()) {
+            renderer.prepare();
+            renderer.render(model);
             DisplayManager.updateDisplay();
 
         }
+
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
 
