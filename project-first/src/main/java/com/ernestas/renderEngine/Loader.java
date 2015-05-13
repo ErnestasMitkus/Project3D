c2e5f34265c0f6e81cb734c295745b8cb586c761
Ernestas Mitkus <ernestas@inventi.lt>
2015-05-13 20:39:23 +0300

    Mipmap the textures

--- project-first/src/main/java/com/ernestas/renderEngine/Loader.java
+++ project-first/src/main/java/com/ernestas/renderEngine/Loader.java
@@ -3,10 +3,7 @@ package com.ernestas.renderEngine;
 import com.ernestas.models.RawModel;
 import com.ernestas.objConverter.ModelData;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -41,6 +38,9 @@ public class Loader {
         Texture texture = null;
         try {
             texture = TextureLoader.getTexture("PNG", new FileInputStream("textures/" + fileName + ".png"));
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+            GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
         } catch (IOException e) {
             e.printStackTrace();
         }
