4c2591a2be536044aee466dffefcaf95db82228f
Ernestas Mitkus <ernestas@inventi.lt>
2015-05-14 09:21:27 +0300

    Make skybox rotate

--- project-first/src/main/java/com/ernestas/shaders/SkyboxShader.java
+++ project-first/src/main/java/com/ernestas/shaders/SkyboxShader.java
@@ -1,6 +1,7 @@
 package com.ernestas.shaders;
 
 import com.ernestas.entities.Camera;
+import com.ernestas.renderEngine.DisplayManager;
 import com.ernestas.toolbox.Maths;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
@@ -10,9 +11,13 @@ public class SkyboxShader extends ShaderProgram {
     private static final String VERTEX_FILE = "shaders/skyboxVertexShader.txt";
     private static final String FRAGMENT_FILE = "shaders/skyboxFragmentShader.txt";
 
+    private static final float ROTATE_SPEED = 1;
+
     private int locationProjectionMatrix;
     private int locationViewMatrix;
     private int locationFogColor;
+
+    private float rotation = 0;
 
     public SkyboxShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -25,8 +30,12 @@ public class SkyboxShader extends ShaderProgram {
     public void loadViewMatrix(Camera camera){
         Matrix4f matrix = Maths.createViewMatrix(camera);
         matrix.m30 = 0; //Make skybox not move with the world
-        matrix.m31 = 0; //camera moves in the completely oposite direction of the world
+        matrix.m31 = 0; //camera moves in the completely opposite direction of the world
         matrix.m32 = 0;
+        rotation += ROTATE_SPEED * DisplayManager.getFrameTimeSeconds();
+        // We should rotate the transformation matrix, but since we are not using the needed columns of the view matrix
+        //  we might as well do the transformation directly on the view matrix
+        Matrix4f.rotate((float) Math.toRadians(rotation), new Vector3f(0, 1, 0), matrix, matrix);
         super.loadMatrix(locationViewMatrix, matrix);
     }
 
